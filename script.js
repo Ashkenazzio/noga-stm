65a75afd7adacd337147c2e954466d893a6ad83f
XanderTEM <98898546+XanderTEM@users.noreply.github.com>
2025-02-08 14:24:38 +0200

    finalized

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const swiper = new Swiper('.swiper', {
-  spaceBetween: 16, // Space between slides
+  spaceBetween: 16,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
   breakpoints: {
     768: {
-      // Large screens
-      slidesPerView: 2.5,
+      slidesPerView: 'auto',
     },
   },
 });
