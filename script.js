6dd6c018efd867f00929d0c25b7a29ddb10a7eaa
XanderTEM <98898546+XanderTEM@users.noreply.github.com>
2025-02-10 19:25:38 +0200

    some small fixes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const swiper = new Swiper('.swiper', {
   spaceBetween: 16,
+  loop: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -10,3 +11,10 @@ const swiper = new Swiper('.swiper', {
     },
   },
 });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const image = document.querySelector('.method-card__img--image.second');
+//   image.onload = function () {
+//     image.style.top = '-145%'; // Apply positioning after load
+//   };
+// });
